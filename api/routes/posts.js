ae372d0e25ac10321f508482e84bfa0b4b99b953
Joaos98 <joao.sousa@edu.unirio.br>
2019-11-16 14:27:10 -0300

    Tanaka eu te odeio

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -5,46 +5,49 @@ const Denuncia = require(process.cwd() + '/models/denunciaModel.js'); //Importan
 
 //Rota para criar uma denúncia de um post no banco de dados
 //Recebe um objeto com os atributos do post específico
+router.post('/criarPost', (req, res) => {
+  postgres.query('INSERT INTO post (id_usuario, id_meme, CURRENT_DATE, conteudo, urlimgur, idimgur) values ()',
+      [], (err, resultado) => {
+    if (err){
+      res.status(400).send(err);
+    }else{
+      res.status(200).send();
+    }
+      });
+});
 router.post('/denunciarPost', (req, res) => {
-  const novaDenuncia = new Denuncia({
-    postID: req.body.idPost,
-    usuarioID: req.body.idUsuario,
-    conteudo: req.body.conteudo,
-    postUrlImgur: req.body.urlImgur,
-    postConteudo: req.body.conteudoPost,
-    idImgur: req.body.idImgur
+  postgres.query('INSERT INTO post (id_post, id_usuario, id_meme, CURRENT_DATE, conteudo, urlimgur, idimgur) values ()')
+  postgres.query('INSERT INTO denuncias (id_post, conteudo) values ($1, $2)',
+      [req.body.idPost, req.body.conteudo], (err, resultado) => {
+    if (err){
+      res.status(400).send(err);
+    }else{
+      res.status(200).send();
+    }
   });
-  novaDenuncia
-    .save()
-    .then(() => {
-      res.status(200).send('Denúncia salva com sucesso.');
-    })
-    .catch(err => {
-      console.log(err.message);
-      res.status(400).send('Problema salvando denúncia.');
-    });
 });
 
 //Rota para obter todas as denúncias no banco de dados
 //Retorna um array com as denúncias encontradas
 router.get('/denuncias', (req, res) => {
-  Denuncia.find({}, (err, denuncias) => {
-    res.status(200).send(denuncias);
-  }).catch(err => {
-    console.log('Erro ao buscar as denúncias no BD:' + err.message);
-    res.status(400).send('Erro ao buscar as denúncias no BD!');
+  postgres.query('SELECT * FROM denuncias', [], (err, resultado) => {
+    if (err){
+      res.status(400).send(err);
+    }else{
+      res.status(200).send(resultado.rows);
+    }
   });
 });
 
 //Rota para deletar denúncias utilizando uma query recebida através da URL
 router.delete('/deletarDenuncias', (req, res) => {
-  Denuncia.deleteMany(req.query)
-    .then(() => {
-      res.status(200).send('Denuncia deletada com sucesso.');
-    })
-    .catch(err => {
-      res.status(400).send('Erro ao deletar denuncia.');
-    });
+  postgres.query('DELETE FROM denuncias WHERE id_denuncia = $1', [req.query._id], (err, resultado) => {
+    if (err){
+      res.status(400).send(err);
+    }else{
+      res.status(200).send();
+    }
+  });
 });
 
 module.exports = router;
